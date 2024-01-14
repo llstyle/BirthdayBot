7d46de40562e4d8dc2a175bcc14c224a963605e1
llstyle <weredhol@gmail.com>
2024-01-14 15:24:02 +0200

    update

--- index.js
+++ index.js
@@ -11,26 +11,25 @@ const userId = process.env.USER_ID
 const PORT = 3000
 
 const app = express();
-const bot = new TelegramBot(TOKEN);
-bot.setWebHook(`${URL}/api/${TOKEN}`)
 
-cron.schedule('0 * * * *', async () => {
-    try {
-        const response = await axios.get(`https://www.rada.zp.ua/pozdravleniya/pozdravleniya-s-dnem-rozhdeniya/s-dnem-rozhdeniya-svoimi-slovami`)
-        const $ = cheerio.load(response.data)
-        const l = $("p").eq(count).text()
-        count++
-        bot.sendMessage(userId, l)
-    } catch(e) {
-        console.log(e)
-    }
-})
-
-app.post(`/api/${TOKEN}` ,(req, res) =>{
+app.post(`/api/${TOKEN}`, (req, res) =>{
     bot.processUpdate(req.body);
     res.sendStatus(200);
 });
 
 app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT))
 
+const bot = new TelegramBot(TOKEN);
+bot.setWebHook(`${URL}/api/${TOKEN}`)
 
+cron.schedule('30 * * * *', async () => {
+    try {
+        const response = await axios.get(`https://www.rada.zp.ua/pozdravleniya/pozdravleniya-s-dnem-rozhdeniya/s-dnem-rozhdeniya-svoimi-slovami`)
+        const $ = cheerio.load(response.data)
+        const text = $("p").eq(count).text()
+        count++
+        bot.sendMessage(userId, text)
+    } catch(e) {
+        console.log(e)
+    }
+})
